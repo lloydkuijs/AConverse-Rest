118a6de2543eccbbc0589a302b018e6cf15ec6cb
lloydkuijs <lloydkuijs@outlook.com>
2020-06-01 09:02:26 +0200

    Removed format from the transcribe request since we have no clue how the transfer is going to work at the current time.

--- Models/Converse/TranscribeRequest.cs
+++ Models/Converse/TranscribeRequest.cs
@@ -2,7 +2,7 @@ namespace AConverse_Rest.Models.Converse
 {
     public class TranscribeRequest
     {
-        public byte[] Audio { get; set; }
-        public string Format { get; set; }
+        public string Audio { get; set; }
+        //public string Format { get; set; }
     }
 }
